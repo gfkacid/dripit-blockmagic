0c3f0322cdbbdaa31fc4dadaa3266dea383f8ea7
Hendobox <50964581+Hendobox@users.noreply.github.com>
2024-05-09 10:26:58 +0100

    fixed option id type, added prediction, resolution, and dummy oracle functions

--- smart-contracts/src/Battles.sol
+++ smart-contracts/src/Battles.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.20;
 import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import "openzeppelin-contracts/contracts/utils/Context.sol";
 
-enum BattleMode {
+enum BattleType {
     Artist,
     Track
 }
@@ -16,8 +16,8 @@ enum BattleOption {
 }
 
 struct BattleManifest {
-    uint256 option0Id;
-    uint256 option1Id;
+    string option0Id;
+    string option1Id;
 }
 
 struct UserPrediction {
@@ -28,30 +28,27 @@ struct UserPrediction {
 
 struct BattleData {
     address creator;
-    BattleMode mode;
+    BattleType battleType;
     BattleManifest manifest;
-    uint256 amountRaised;
+    uint256 prizePool;
     uint256 option0Count;
     uint256 option1Count;
     uint16 startTimestamp;
     uint16 closeTimestamp;
     uint256 aPIRequestId;
-    BattleOption winner;
+    BattleOption winOption;
 }
 
 contract Battles is Context {
     using SafeERC20 for IERC20;
 
     uint256 public battleIds;
-    uint256 public minCreationStake;
+    uint256 public minBetAmount;
     uint16 public duration;
     IERC20 public token;
 
     // user => battleId => UserPrediction
     mapping(address => mapping(uint256 => UserPrediction)) _userToIdToPrediction;
-
-    // user => UserPredictions
-    mapping(address => UserPrediction[]) _userToPredictions;
 
     // battleId => BattleData
     mapping(uint256 => BattleData) public battles;
@@ -64,32 +61,45 @@ contract Battles is Context {
 
     event CreateBattle(
         address indexed creator,
-        BattleMode indexed mode,
-        uint256 id,
+        BattleType indexed battleType,
+        uint256 battleId,
         bytes32 indexed hash,
-        BattleManifest manifest,
-        uint256 amount
+        BattleManifest manifest
     );
 
-    constructor(address _token, uint256 _minCreationStake, uint16 _duration) {
+    event MakePrediction(
+        address indexed player,
+        uint256 indexed battleId,
+        uint256 amount,
+        BattleOption indexed option
+    );
+
+    event ResolveBattle(
+        uint256 indexed battleId,
+        BattleOption indexed winOption,
+        uint256 indexed aPIRequestId,
+        uint256 timestamp
+    );
+
+    constructor(address _token, uint256 _minBetAmount, uint16 _duration) {
         token = IERC20(_token);
-        minCreationStake = _minCreationStake;
+        minBetAmount = _minBetAmount;
         duration = _duration;
     }
 
-    modifier onlyOption(BattleOption option) {
+    modifier validBet(BattleOption option, uint256 amount) {
         require(BattleOption.Nil != option, "Error: Wrong option");
+        require(amount >= minBetAmount, "Error: Inssuficient amount input");
         _;
     }
 
     function createBattle(
         BattleManifest calldata manifest,
-        BattleMode mode,
+        BattleType battleType,
         BattleOption option,
         uint256 amount
-    ) external onlyOption(option) {
-        require(amount >= minCreationStake, "Error: Inssuficient amount input");
-        (bytes32 hash, bool active) = generateHash(manifest);
+    ) external validBet(option, amount) {
+        (bytes32 hash, bool active) = generateHash(manifest, battleType);
         require(!active, "Error: Manifest is already active");
 
         token.safeTransferFrom(_msgSender(), address(this), amount);
@@ -107,27 +117,78 @@ contract Battles is Context {
 
         battles[id] = BattleData({
             creator: _msgSender(),
-            mode: mode,
+            battleType: battleType,
             manifest: manifest,
-            amountRaised: amount,
+            prizePool: amount,
             option0Count: option0Count,
             option1Count: option1Count,
             startTimestamp: uint16(block.timestamp),
             closeTimestamp: uint16(block.timestamp) + duration,
             aPIRequestId: 0,
-            winner: BattleOption.Nil
+            winOption: BattleOption.Nil
         });
 
         _creatorToBattleIds[_msgSender()].push(id);
 
-        emit CreateBattle(_msgSender(), mode, id, hash, manifest, amount);
+        emit CreateBattle(_msgSender(), battleType, id, hash, manifest);
+        emit MakePrediction(_msgSender(), id, amount, option);
     }
 
     function makePrediction(
         uint256 battleId,
         BattleOption option,
         uint256 amount
-    ) external onlyOption(option) {}
+    ) external validBet(option, amount) {
+        BattleData storage battle = battles[battleId];
+        require(
+            battle.startTimestamp > uint16(block.timestamp),
+            "Error: Entry is not open"
+        );
+
+        token.safeTransferFrom(_msgSender(), address(this), amount);
+
+        unchecked {
+            battle.prizePool += amount;
+            if (option == BattleOption.Option0) {
+                battle.option0Count++;
+            } else {
+                battle.option1Count++;
+            }
+        }
+
+        emit MakePrediction(_msgSender(), battleId, amount, option);
+    }
+
+    function resolveBattle(uint256 battleId) external {
+        BattleData memory battleMem = battles[battleId];
+        require(
+            battleMem.prizePool > 0 &&
+                uint16(block.timestamp) >= battleMem.closeTimestamp,
+            "Error: Invalid operation"
+        );
+        BattleData storage battleSto = battles[battleId];
+        (BattleOption winOption, uint256 aPIRequestId) = _dummyChainlinkFunc(
+            battleMem.startTimestamp,
+            battleMem.closeTimestamp
+        );
+
+        battleSto.winOption = winOption;
+        battleSto.aPIRequestId = aPIRequestId;
+
+        emit ResolveBattle(battleId, winOption, aPIRequestId, block.timestamp);
+    }
+
+    function _dummyChainlinkFunc(
+        uint16 start,
+        uint16 close
+    ) private view returns (BattleOption, uint256) {
+        uint256 aPIRequestId = close - start + block.timestamp;
+        if (uint16(block.timestamp) < close)
+            return (BattleOption.Nil, aPIRequestId);
+        if ((close - start) % 2 == 0)
+            return (BattleOption.Option0, aPIRequestId);
+        return (BattleOption.Option1, aPIRequestId);
+    }
 
     function getCreations(
         address creator
@@ -152,27 +213,24 @@ contract Battles is Context {
         return battles[_creatorToBattleIds[creator][index]];
     }
 
-    function getPredictions(
-        address user
-    ) external view returns (UserPrediction[] memory) {
-        return _userToPredictions[user];
-    }
-
-    function getPredictionByIndex(
+    function getPredictionByBattleId(
         address user,
-        uint256 index
+        uint256 battleId
     ) external view returns (UserPrediction memory) {
-        return _userToIdToPrediction[user][index];
+        return _userToIdToPrediction[user][battleId];
     }
 
     function generateHash(
-        BattleManifest calldata manifest
+        BattleManifest calldata manifest,
+        BattleType battleType
     ) public view returns (bytes32 hash, bool) {
         // Sort from smallest to biggest
-        (uint256 a, uint256 b) = manifest.option0Id < manifest.option1Id
+        (string memory a, string memory b) = keccak256(
+            abi.encodePacked(manifest.option0Id)
+        ) < keccak256(abi.encodePacked(manifest.option1Id))
             ? (manifest.option0Id, manifest.option1Id)
             : (manifest.option1Id, manifest.option0Id);
-        hash = keccak256(abi.encodePacked(a, b));
+        hash = keccak256(abi.encodePacked(a, b, battleType));
         return (hash, _isActive[hash]);
     }
 }
