cf20b47535210c30d3a24b89214760b7864ca4c8
aleks <aleksandar@kalaydzhiev.com>
2024-05-20 20:14:32 +0300

    Updated Table structure

--- Database/Tables_Query_script.sql
+++ Database/Tables_Query_script.sql
@@ -1,35 +1,35 @@
 USE dripit;
 
-CREATE TABLE dbo.users (
-	id INT IDENTITY (1, 1) PRIMARY KEY,
-	email VARCHAR (50) NOT NULL,
-	login_type INT NOT NULL,
-	username VARCHAR (50) NOT NULL,
-	avatar VARCHAR (1000),
-	wallet CHAR (42) NOT NULL,
-	referred_by INT,
-	auth_identifier VARCHAR (100) NOT NULL
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR (50) NOT NULL,
+    login_type INT NOT NULL,
+    username VARCHAR (50) NOT NULL,
+    avatar VARCHAR (1000),
+    wallet CHAR (42) NOT NULL,
+    referred_by INT,
+    auth_identifier VARCHAR (100) NOT NULL
 );
 
-CREATE TABLE dbo.artists (
-	id INT IDENTITY (1, 1) PRIMARY KEY,
-	spotify_id VARCHAR (50) NOT NULL,
-	name VARCHAR (50) NOT NULL,
-	slug VARCHAR (50) NOT NULL,
-	image VARCHAR (1000) NOT NULL,
-	bio TEXT
+CREATE TABLE artists (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    spotify_id VARCHAR (50) NOT NULL,
+    name VARCHAR (50) NOT NULL,
+    slug VARCHAR (50) NOT NULL,
+    image VARCHAR (1000) NOT NULL,
+    bio TEXT
 );
 
-CREATE TABLE dbo.battles (
-    id INT IDENTITY (1, 1) PRIMARY KEY,
+CREATE TABLE battles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     type TINYINT NOT NULL,
     status TINYINT NOT NULL,
     sideA_id INT NOT NULL,
     FOREIGN KEY (sideA_id)
-        REFERENCES dbo.artists(id),
+        REFERENCES artists(id),
     sideB_id INT NOT NULL,
     FOREIGN KEY (sideB_id)
-        REFERENCES dbo.artists(id),
+        REFERENCES artists(id),
     amountA BIGINT NOT NULL,
     amountB BIGINT NOT NULL,
     total BIGINT NOT NULL,
@@ -37,54 +37,55 @@ CREATE TABLE dbo.battles (
     end_date DATETIME NOT NULL,
     created_by INT NOT NULL,
     FOREIGN KEY (created_by)
-        REFERENCES dbo.users(id)
+        REFERENCES users(id)
 );
 
-CREATE TABLE dbo.positions (
-	id INT IDENTITY (1, 1) PRIMARY KEY,
-	battle_id INT NOT NULL,
-	FOREIGN KEY (battle_id)
-		REFERENCES dbo.battles(id)
-		ON DELETE NO ACTION ON UPDATE CASCADE,
-	user_id INT NOT NULL,
-	FOREIGN KEY (user_id)
-		REFERENCES dbo.users(id)
-		ON DELETE NO ACTION ON UPDATE CASCADE,
-	side BIT NOT NULL,
-	amount BIGINT NOT NULL,
-	created_at DATETIME NOT NULL,
-	won BIT NOT NULL,
-	updated_at DATETIME,
+CREATE TABLE positions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    battle_id INT NOT NULL,
+    FOREIGN KEY (battle_id)
+        REFERENCES battles(id)
+        ON DELETE NO ACTION ON UPDATE CASCADE,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE NO ACTION ON UPDATE CASCADE,
+    side BIT NOT NULL,
+    amount BIGINT NOT NULL,
+    created_at DATETIME NOT NULL,
+    won BIT NOT NULL,
+    updated_at DATETIME
 );
 
-CREATE TABLE dbo.claims (
-	id INT IDENTITY (1, 1) PRIMARY KEY,
-	position_id INT NOT NULL,
-	FOREIGN KEY (position_id)
-		REFERENCES dbo.positions(id),
-	amount BIGINT,
-	claimed_at DATETIME,
-	user_id INT NOT NULL,
-	FOREIGN KEY (user_id)
-		REFERENCES dbo.users(id)
+CREATE TABLE claims (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    position_id INT NOT NULL,
+    FOREIGN KEY (position_id)
+        REFERENCES positions(id),
+    amount BIGINT,
+    claimed_at DATETIME,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
 );
 
-CREATE TABLE dbo.artist_spotify_stats (
-	id INT IDENTITY (1, 1) PRIMARY KEY,
-	artist_id INT NOT NULL,
-	FOREIGN KEY (artist_id)
-		REFERENCES dbo.artists(id),
-	total_plays INT NOT NULL,
-	followers INT NOT NULL,
-	updated_at DATETIME
+CREATE TABLE artist_spotify_stats (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    artist_id INT NOT NULL,
+    FOREIGN KEY (artist_id)
+        REFERENCES artists(id),
+    total_plays INT NOT NULL,
+    followers INT NOT NULL,
+    updated_at DATETIME
 );
 
-CREATE TABLE dbo.spodity_weekly_pllays (
-	id INT IDENTITY (1, 1) PRIMARY KEY,
-	artist_id INT NOT NULL,
-	FOREIGN KEY (artist_id)
-		REFERENCES dbo.artists(id),
-	total_plays INT NOT NULL,
-	start_date DATETIME NOT NULL,
-	end_date DATETIME NOT NULL,
+CREATE TABLE spodity_weekly_plays (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    artist_id INT NOT NULL,
+    FOREIGN KEY (artist_id)
+        REFERENCES artists(id),
+    total_plays INT NOT NULL,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME NOT NULL
 );
+
